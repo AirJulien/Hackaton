34dce7a7ba4794cf35d3dcb9a28c969521b0ed2c
AirJulien <julien.gueperoux@gmail.com>
2018-10-13 08:58:23 +0200

    refonte de l'environnement

---
+++ RecoObject/src/org/tensorflow/demo/TTSListener.java
@@ -0,0 +1,41 @@
+
+package org.tensorflow.demo;
+
+import android.content.Context;
+import android.speech.tts.TextToSpeech;
+
+public class TTSListener implements TextToSpeech.OnInitListener {
+
+    private TextToSpeech tts;
+    private boolean ttsOk;
+
+    // The constructor will create a TextToSpeech instance.
+    TTSListener(Context context) {
+        tts = new TextToSpeech(context, this);
+    }
+
+    @Override
+    // OnInitListener method to receive the TTS engine status
+    public void onInit(int status) {
+        if (status == TextToSpeech.SUCCESS) {
+            ttsOk = true;
+        }
+        else {
+            ttsOk = false;
+        }
+    }
+
+    // A method to speak something
+    @SuppressWarnings("deprecation") // Support older API levels too.
+    public void speak(String text, Boolean override) {
+        if (ttsOk) {
+            if (override) {
+                tts.speak(text, TextToSpeech.QUEUE_FLUSH, null);
+            }
+            else {
+                tts.speak(text, TextToSpeech.QUEUE_ADD, null);
+            }
+        }
+    }
+
+}
